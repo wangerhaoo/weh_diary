5f0ae5ef442bc2bbfd3ed4ecc3a43e3f43764943
wangerhao <1532531258@qq.com>
2022-11-05 16:58:33 +0800

    project init

---
+++ src/main/java/com/weh/weh_diary/WehDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.weh.weh_diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WehDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WehDiaryApplication.class, args);
+	}
+
+}
